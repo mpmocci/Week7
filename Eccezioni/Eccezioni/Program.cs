92955e08bfa25da263116bae737f0d309be7aa05
mpmocci <91893446+mpmocci@users.noreply.github.com>
2021-11-24 08:49:50 +0100

    u

--- Eccezioni/Eccezioni/Program.cs
+++ Eccezioni/Eccezioni/Program.cs
@@ -21,7 +21,7 @@ namespace Eccezioni
             }
 
 
-            //try
+            //try th
             //{
             //    int[] array = new int[10] { 1, 3, 5, 7, 9, 11, 13, 15, 17, 19 };
 
